3e0f604a391a45fb00f120ba405e170b824b4c7a
Sam Graler <samgraler@gmail.com>
2024-07-20 13:30:08 -0400

    random stuff

---
+++ src/hooks/useClientSide.ts
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react';
+
+const useClientSide = () => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  return isClient;
+};
+
+export default useClientSide;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,32 @@
-import { Fragment } from 'react';
-import Head from 'next/head';
+'use client';
+
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import useClientSide from '../hooks/useClientSide';
+
 import './global.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <Fragment>
-      <Head>
-        <title>tracr.io</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-      </Head>
-      <Component {...pageProps} />
-    </Fragment>
-  );
+const publicPaths = ['/login', '/signup', '/supplier-auth']; // Add public paths here
+let userId: number | null = null;
+
+export function setUserId(id: number) {
+  userId = id;
 }
 
-export default MyApp;
+export function getUserId() {
+  return userId;
+}
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const isClient = useClientSide();
+
+  useEffect(() => {
+    if (isClient) {
+      router.push('/login');
+    }
+  }, [router, isClient]);
+
+  return <Component {...pageProps} />;
+}
